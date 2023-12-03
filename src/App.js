f2ac79f6f83b805e39b17126bbaab38e0917f5a1
Ashwath Naik <anaik98@gmail.com>
2023-12-03 00:48:07 -0500

    Title updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ashwath and Sowmya's App
+          Ammi Kutti Version 2.0
         </p>
         <a
           className="App-link"
